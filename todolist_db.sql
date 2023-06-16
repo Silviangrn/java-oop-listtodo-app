468122b524b4bae0376f8b773a7b986b9fc3e87c
Silviangrn <silvia.angrn@gmail.com>
2023-06-16 23:21:59 +0700

    Update db

--- todolist_db.sql
+++ todolist_db.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Bulan Mei 2023 pada 04.00
--- Versi server: 10.3.16-MariaDB
--- Versi PHP: 7.3.8
+-- Waktu pembuatan: 16 Jun 2023 pada 18.16
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,6 +34,15 @@ CREATE TABLE `task` (
   `created_at` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `task`
+--
+
+INSERT INTO `task` (`id`, `task`, `userid`, `created_at`) VALUES
+(2, '08:00 Sarapan', 'Silvia', '2023-06-16'),
+(3, '09:00 Cuci baju', 'Silvia', '2023-06-16'),
+(4, '22:00 Tidur', 'Silvia', '2023-06-16');
+
 -- --------------------------------------------------------
 
 --
@@ -42,10 +50,17 @@ CREATE TABLE `task` (
 --
 
 CREATE TABLE `user` (
-  `userid` int(20) NOT NULL,
-  `password` int(20) NOT NULL,
+  `userid` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
   `created_at` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`userid`, `password`, `created_at`) VALUES
+('Silvia', '1234', '2023-06-16');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +80,7 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT untuk tabel `task`
 --
 ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
